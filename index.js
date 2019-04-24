78808f20506511e40b3fe58e40a6798526c8415e
Ryan Bravo <rrbravo2@gmail.com>
2019-04-24 15:38:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+let katzDeli = [];
+
+function takeANumber (KatzDeli, name) {
+  KatzDeli.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${KatzDeli.length} in line.`);
+}
+
+function nowServing (KatzDeli) {
+  let i = 0;
+  while (i < KatzDeli.length) {
+    i++;
+  }
+  if (KatzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else 
+  return (`Currently serving ${KatzDeli.shift()}.`);
+}
+
+var line = [];
+function currentLine(KatzDeli) {
+  for (let i = 0; i < KatzDeli.length; i++){
+    line.push(` `+[i+1]+`. `+ KatzDeli[i]);
+  }
+  if (KatzDeli.length === 0) {
+    return "The line is currently empty.";
+  }else
+  return(`The line is currently:` + line);
+  
+}
